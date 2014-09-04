4517641744f851bbd0b440f2c134ebceaad3fba0
terwer <cbgtyw@gmail.com>
2014-09-04 11:09:38 +0800

    验证样式兼容ie6.

--- teaCRM.Web/Themes/default/crm/js/customer_add.js
+++ teaCRM.Web/Themes/default/crm/js/customer_add.js
@@ -3,13 +3,74 @@
 //*时间：2014年09月01日
 
 $(document).ready(function() {
-    validate_form();
+    //加载省市数据
+    load_city_data();
 });
 
 
 $(function() {
-
+    validate_form();
 });
+
+//===============================================================================================
+//需要引用 <script src="/Themes/default/base/js/city.js" type="text/javascript"></script>
+//加载省市数据 2014-09-04 By 唐有炜
+function load_city_data() {
+    //alert(city_data_2014);
+    //预加载
+    $("#cus_province option").remove();
+    $("#cus_province").prepend("<option value=\"0\">请选择省份</option>"); //为select插入一个option(第一个位置) 
+    $("#cus_city option").remove();
+    $("#cus_city").prepend("<option value=\"0\">请选择城市</option>"); //为select插入一个option(第一个位置) 
+    $("#cus_region option").remove();
+    $("#cus_region").prepend("<option value=\"0\">请选择地区</option>"); //为select插入一个option(第一个位置) 
+    //加载省份
+    $.each(city_data_2014, function(index, field) {
+        var prov_name = field.region.name;
+        var prov_code = field.region.code;
+        $("#cus_province").append("<option value=\"" + prov_code + "\" >" + prov_name + "</option>");
+    });
+    //级联城市
+    $("#cus_province").change(function() {
+        var prov_code = $(this).val();
+        $.each(city_data_2014, function(index, field) {
+            var state = field.region.state;
+            if (prov_code == field.region.code) { //只添加当前省份下的城市
+                $("#cus_city option").remove();
+                $("#cus_city").prepend("<option value=\"0\">请选择城市</option>"); //为select插入一个option(第一个位置) 
+                $.each(state, function(index2, field2) {
+                    var city_name = field2.name;
+                    var city_code = field2.code;
+                    $("#cus_city").append("<option value=\"" + city_code + "\" >" + city_name + "</option>");
+                });
+            }
+        });
+    });
+    //级联地区
+    $("#cus_city").change(function() {
+        var prov_code = $("#cus_province").val();
+        var city_code = $(this).val();
+        $.each(city_data_2014, function(index, field) {
+            var state = field.region.state;
+            if (prov_code == field.region.code) { //只添加当前省份下的城市
+                $.each(state, function(index2, field2) {
+                    var city = field2.city;
+                    if (city_code == field2.code) {
+                        $("#cus_region option").remove();
+                        $("#cus_region").prepend("<option value=\"0\">请选择地区</option>"); //为select插入一个option(第一个位置) 
+                        $.each(city, function(index3, field3) {
+                            var region_name = field3.name;
+                            var region_code = field3.code;
+                            $("#cus_region").append("<option value=\"" + region_code + "\" >" + region_name + "</option>");
+                        });
+                    }
+                });
+            }
+        });
+    });
+}
+
+//=======================================================================================================
 
 //表单验证
 function validate_form() {
@@ -17,23 +78,41 @@ function validate_form() {
     $("#form_customer").validate({
         rules: {
             cus_name: {
-                rangelength: [2, 20]
+                rangelength: [2, 200]
+            },
+            cus_sname: {
+                rangelength: [1, 100]
             },
             cus_tel: {
                 remote: { type: "POST", url: '/Apps/CRM/LoadData/ValidatePhone/' }
+            },
+            cus_address: {
+                maxlength: 255
+            },
+            cus_note: {
+                maxlength: 255
             }
         },
         messages: {
             cus_name: {
                 required: "客户名称不能为空！",
-                rangelength: "客户名称长度必须在2-20之间"
+                rangelength: "客户名称长度必须在2-200之间"
+            },
+            cus_sname: {
+                rangelength: "助记简称长度必须在1-100之间"
             },
             cus_tel: {
                 required: "电话或手机号码不能为空！",
                 remote: "该电话或手机号码已存在！"
             },
+            cus_address: {
+                maxlength: "详细地址最长只能为255位"
+            },
+            cus_note: {
+                maxlength: "备注最多只能为255位"
+            },
             con_name: {
-                required: "主联系人名称不能为空！",
+                required: "主联系人名称不能为空！"
             }
         },
 
